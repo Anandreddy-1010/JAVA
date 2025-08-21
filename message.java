eb0e3dad31c2c10e3026138d06a98c519ea02830
Anandreddy-1010 <kanandr7@gmail.com>
2025-08-21 18:05:14 +0530

    Add files via upload

---
+++ message.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class message {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name : ");
+        String s = sc.next();
+        System.out.println("enter the age : ");
+        int a = sc.nextInt();
+        System.out.println("Hi "+s + " You are "+a + " years old" );
+
+
+
+
+
+
+    }
+}
