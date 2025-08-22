bd594d1c454aa05253bfe59e1c9c959174d62ee1
Anandreddy-1010 <kanandr7@gmail.com>
2025-08-22 19:13:38 +0530

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,18 @@
+class calculator{
+    int a;
+    public int add(int n1,int n2){
+        int r=n1+n2;
+        return r;
+    }
+    
+}
+class cal {
+    public static void main(String[] args) {
+        int num1=4;
+        int num2=5;
+        calculator calc=new calculator();
+        int result=calc.add(num1,num2);
+        System.out.println(result);
+       
+    }
+}
