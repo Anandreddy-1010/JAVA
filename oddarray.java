d45e4c46ed11b4c01c3e2ae9b8985740f1880a0e
Anandreddy-1010 <kanandr7@gmail.com>
2025-09-04 23:00:54 +0530

    Add files via upload

---
+++ oddarray.java
@@ -0,0 +1,12 @@
+class oddarray {
+    public static void main(String[] args) {
+        int[] arr={2,3,4,5,6,7,8,9,10};
+        System.out.println("odd numbers in the array:");
+        for(int num:arr){
+            if(num%2 !=0){
+                System.out.println(num +" ");
+            }
+        }
+    }
+    
+}
